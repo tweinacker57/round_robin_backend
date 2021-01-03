de42f55bef6223a796465bd5398eaeeb9101e6bc
Tyler Weinacker <tweinacker57@gmail.com>
2021-01-03 11:18:00 -0500

    Add index and show action to the User controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,5 +1,12 @@
 class Api::UsersController < ApplicationController
   def index
+    @users = User.all
     render "index.json.jb"
+  end
+
+  def show
+    id = params[:id]
+    @user = User.find_by(id: id)
+    render 'show.json.jb'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,5 +9,6 @@ Rails.application.routes.draw do
 
   namespace :api do
     get "/users" => "users#index"
+    get "/users/:id" => "users#show"
   end
 end
